5d5b48316275e56262e93a3cbee03beb003c891f
Sangwon Lee <leee.sangwon@gmail.com>
2017-12-13 06:58:26 +0900

    MODIFY the code to save the result csv files

--- ImageTypeSelector.py
+++ ImageTypeSelector.py
@@ -2,7 +2,7 @@ import sys, os
 import glob
 import pandas as pd
 import shutil
-import datetime
+import time
 
 from PyQt5 import QtGui, QtCore
 from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox
@@ -15,48 +15,51 @@ class XMainWindow(QMainWindow, selectorUI.Ui_MainWindow):
         self.setupUi(self)
 
         self.nextButton.clicked.connect(self.nextImage)
-        self.saveButton.clicked.connect(self.saveResult)
         self.benignButton.clicked.connect(self.radioButtonClicked)
         self.cancerButton.clicked.connect(self.radioButtonClicked)
         
-        self.inputData = InputDataHandler("data")
-        self.selectionResults = SelectionResultsHandler(self.inputData)
+        self.out_path = 'results'
+        if not os.path.isdir(self.out_path):
+            os.makedirs(self.out_path)
+        self.in_path = 'data'
+        self.input_data = InputDataHandler(self.in_path)
+        self.selection_results = SelectionResultsHandler(self.input_data, self.out_path)
         self.restore()
         self.current_selection = "benign"
-        self.updateImage()    
+        self.updateImage()
 
     def restore(self):
-        if os.path.isfile(self.selectionResults.out_path):
-            self.selectionResults.restoreFromPreviousWorks()
-            self.inputData.restoreFromPreviousWorks(self.selectionResults.current_data_index_list)
-            self.loadDataset(self.inputData.current_folder_index)
+        if os.path.isfile(self.selection_results.backup_path):
+            self.selection_results.restoreFromPreviousWorks()
+            self.input_data.restoreFromPreviousWorks(self.selection_results.current_data_index_list)
+            self.loadDataset(self.input_data.current_folder_index)
 
     def closeEvent(self, event):
-        self.selectionResults.export()
+        self.saveResult()
 
     def nextImage(self):
         assert self.current_selection in ["benign", "cancer"], "Invalid selection: %s" % (self.current_selection)
-        self.selectionResults.saveSelection(self.inputData.current_folder_index, self.inputData.currentDataIndex(), self.current_selection)
+        self.selection_results.saveSelection(self.input_data.current_folder_index, self.input_data.currentDataIndex(), self.current_selection)
         try:
-            self.inputData.nextData()
+            self.input_data.nextData()
         except IndexError:
             QMessageBox.information(self, 'Last Image', "This is the last image of the current dataset.\nSwitch to the next dataset.", QMessageBox.Ok, QMessageBox.Ok)
-            self.loadDataset(self.inputData.current_folder_index + 1)
+            self.loadDataset(self.input_data.current_folder_index + 1)
         else:
             self.updateImage()
-    
+
     def updateImage(self):
         self.showImage()
-        self.currentImageIndex.setText(str(self.inputData.currentDataIndex()+1))
+        self.currentImageIndex.setText(str(self.input_data.currentDataIndex()+1))
 
     def showImage(self):
-        path = self.inputData.currentDataName()
+        path = self.input_data.currentDataName()
         assert os.path.isfile(path), "No accessible file: %s" % path
         tumor = QtGui.QPixmap(path)
         self.tumorImage.setPixmap(tumor.scaled(self.tumorImage.width(), self.tumorImage.height(), QtCore.Qt.KeepAspectRatio))
 
     def saveResult(self):
-        self.selectionResults.export()
+        self.selection_results.export()
 
     def radioButtonClicked(self):
         if self.cancerButton.isChecked():
@@ -66,12 +69,12 @@ class XMainWindow(QMainWindow, selectorUI.Ui_MainWindow):
 
     def loadDataset(self, num):
         try:
-            self.inputData.changeDataset(num)
+            self.input_data.changeDataset(num)
         except IndexError("Dataset you choose is already done."):
             QMessageBox.information(self, 'Last Image', "Dataset you choose is already done.", QMessageBox.Ok, QMessageBox.Ok)
         else:
-            self.curDataset.setText(str(self.inputData.current_folder_index))
-            self.numOfWholeImages.setText(str(self.inputData.numberOfCurrentData()))
+            self.curDataset.setText(str(self.input_data.current_folder_index))
+            self.numOfWholeImages.setText(str(self.input_data.numberOfCurrentData()))
             self.updateImage()
     
 
@@ -122,23 +125,25 @@ class InputDataHandler():
     
 
 class SelectionResultsHandler():
-    def __init__(self, inputDataHandler):
-        self.out_path = "result.csv"
+    def __init__(self, inputDataHandler, out_folder):
+        assert os.path.isdir(out_folder), "out_folder does not exist: %s" % out_folder
+        self.backup_path = "backup.imagetypeselector"
+        self.result_file_pattern = os.path.join(out_folder, "result_%d%s.csv")
         self.num_folderes = 15
         self.table_schema = ["folder", "file", "selection"]
         assert len(inputDataHandler.number_of_data_list) == self.num_folderes, "The number of the output folder should be same with the number of the input folder"
         self.number_of_data_list = inputDataHandler.number_of_data_list
         self.current_data_index_list = inputDataHandler.current_data_index_list
-        self.selectionDict = {}
+        self.selection_dict = {}
     
     def restoreFromPreviousWorks(self):
-        assert os.path.isfile(self.out_path), "No accessible file: %s" % self.out_path
-        assert len(self.selectionDict) == 0, "selectionDict does not empty."
-        df = pd.read_csv(self.out_path)
+        assert os.path.isfile(self.backup_path), "No accessible file: %s" % self.backup_path
+        assert not self.selection_dict, "selection_dict does not empty."
+        backup_df = pd.read_csv(self.backup_path)
         folder_to_file_dict = {}
-        for index, row in df.iterrows():
+        for _, row in backup_df.iterrows():
             folder_to_file_dict[row['folder']] = row['file']
-            self.selectionDict[(row['folder'], row['file'])] = row['selection']
+            self.selection_dict[(row['folder'], row['file'])] = row['selection']
         for i in range(self.num_folderes):
             if i in folder_to_file_dict.keys():
                 self.current_data_index_list[i] = folder_to_file_dict[i] + 1
@@ -151,19 +156,30 @@ class SelectionResultsHandler():
         assert folder_idx >= 0 and folder_idx < self.num_folderes, 'Out of bounds'
         assert data_idx >= 0 and data_idx < self.number_of_data_list[folder_idx], 'Out of bounds'
         assert selection in ["benign", "cancer"], "Invalid selction: %s" % selection
-        self.selectionDict[(folder_idx, data_idx)] = selection
+        self.selection_dict[(folder_idx, data_idx)] = selection
 
     def export(self):
-        csv_list = []
-        for key, value in self.selectionDict.items():
+        cur_time = time.localtime()
+        timestr = "%04d%02d%02d_%02d%02d%02d" % (
+            cur_time.tm_year, cur_time.tm_mon, cur_time.tm_mday, cur_time.tm_hour, cur_time.tm_min, cur_time.tm_sec)
+        backup_list = []
+        result_file_list = [[] for _ in range(self.num_folderes)]
+        for key, value in self.selection_dict.items():
             folder = key[0]
             filename = key[1]
             selection = value
             row = (folder, filename, selection)
-            csv_list.append(row)
-        df = pd.DataFrame(csv_list, columns=self.table_schema)
-        df.to_csv(self.out_path)
-
+            backup_list.append(row)
+            result_file_list[folder].append(row[1:])
+        backup_df = pd.DataFrame(backup_list, columns=self.table_schema)
+        backup_df.to_csv(self.backup_path)
+        for i, result_file in enumerate(result_file_list):
+            if result_file:
+                if os.path.exists(self.result_file_pattern % (i, "")):
+                    continue
+                result_df = pd.DataFrame(result_file, columns=self.table_schema[1:])
+                if len(result_file) == self.number_of_data_list[i]: # done dataset
+                    result_df.to_csv(self.result_file_pattern % (i, ""))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
